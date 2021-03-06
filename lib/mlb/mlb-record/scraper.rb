961ea10df413b716873f7a21145afa40e1006264
pbenton6 <patrick.benton6396@gmail.com>
2021-03-06 12:34:04 -0600

    added scraper code

---
+++ lib/mlb/mlb-record/scraper.rb
@@ -0,0 +1,22 @@
+require 'nokogiri'
+require 'open-uri'
+
+require_relative './team.rb'
+
+class Scraper
+    
+    def get_page
+        Nokogiri::HTML(open("https://www.msn.com/en-us/sports/mlb/teams"))
+    end
+
+    def get_teams
+        self.get_page.css("div.teams")
+    end
+
+    def make_teams
+        get_teams.each do |index|
+            Team.new_from_index(index)
+        end
+    end
+
+end
